8cc0ec3d594489849107b2f80dd24588fbf87d6a
Lila <64287938+LilianeSantana@users.noreply.github.com>
2024-07-14 21:21:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//DOCUMENT = busca no HTML e querySelector = busca pelo seletor//
+// querySelector = busca o que eu quiser que ele busque
+
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+    formulario.style.left = "50%"
+}
+
+function desaparecerFormulario(){
+    formulario.style.left = "-50%"
+}
